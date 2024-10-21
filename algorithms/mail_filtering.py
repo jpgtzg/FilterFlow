80e222c9cde9a56b05dc9cdc38e8ddc6ed7aa1e9
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2024-10-21 08:34:50 -0600

    Refactor mail_filtering.py to improve attachment extraction and add filtering capabilities

--- algorithms/mail_filtering.py
+++ algorithms/mail_filtering.py
@@ -4,9 +4,11 @@ import os
 import base64
 import email
 from email.header import decode_header
+import PyPDF2
 
-def get_attached_documents(mail : imaplib.IMAP4_SSL, email_uid : bytes):
+def get_attached_documents(mail : imaplib.IMAP4_SSL, email_uid : bytes) -> int:
     res, msg = mail.uid('fetch', email_uid, "(RFC822)")
+    part_number = 0
     
     for response in msg:
         if isinstance(response, tuple):
@@ -14,7 +16,6 @@ def get_attached_documents(mail : imaplib.IMAP4_SSL, email_uid : bytes):
 
             if msg.is_multipart():
 
-                part_number = 0
                 for part in msg.walk():
                     if part.get_content_disposition() == 'attachment':
                         filename = part.get_filename()
@@ -24,7 +25,6 @@ def get_attached_documents(mail : imaplib.IMAP4_SSL, email_uid : bytes):
                                 os.mkdir("attachments")
 
                             filename = decode_header(filename)[0][0]
-                            
 
                             if isinstance(filename, bytes):
                                 filename = filename.decode()
@@ -32,7 +32,45 @@ def get_attached_documents(mail : imaplib.IMAP4_SSL, email_uid : bytes):
                             ext = filename.split(".")[-1] 
                             part_number += 1
 
-                            filepath = os.path.join("attachments", f'{email_uid.decode()}_attach_{part_number}.{ext}')
+                            filename = f"{email_uid.decode()}_attach_{part_number}.{ext}"
+
+                            filepath = os.path.join("attachments", filename)
                             open(filepath, "wb").write(part.get_payload(decode=True))
                             print(f"Attachment saved: {filename}")
+    
+    return part_number
 
+def apply_filter(mail : imaplib.IMAP4_SSL, email_uid : bytes, filter_function):
+    res, msg = mail.uid('fetch', email_uid, "(RFC822)")
+    
+    for response in msg:
+        if isinstance(response, tuple):
+            msg = email.message_from_bytes(response[1])
+
+            filter_function(msg)
+
+    return None
+
+def filter_pdf_attachment(email_uid: bytes, part_number:int, keywords : list) -> list:
+    if isinstance(keywords, str):
+        keywords = [keywords]
+
+    coincidences = []
+    for i in range(1, part_number+1):
+        filename = f"{email_uid.decode()}_attach_{i}.pdf"
+        filepath = os.path.join("attachments", filename)
+
+        with open(filepath, "rb") as f: 
+            reader = PyPDF2.PdfReader(f)
+            
+            content = ""
+            for page_num in range(len(reader.pages)):
+                content += reader.pages[page_num].extract_text()
+                            
+            for keyword in keywords:
+                if keyword in content:
+                    coincidences.append(filename)
+                    print(f"Keyword found in {filename}: {keyword}")
+                    break
+                    
+    return coincidences
